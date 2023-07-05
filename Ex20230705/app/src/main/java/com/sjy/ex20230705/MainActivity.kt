fe1bcf81d92f9cd44b3398fe4aaa4d4b8cf7278a
sin6338ki <jyshin38@gmail.com>
2023-07-05 18:14:36 +0900

    2023.07.05

--- Ex20230705/app/src/main/java/com/sjy/ex20230705/MainActivity.kt
+++ Ex20230705/app/src/main/java/com/sjy/ex20230705/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.sjy.ex20230705
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState) //준비
+        setContentView(R.layout.activity_main) //xml + kt 연결
+
+        //1. Button 찾기
+        var btn1 : Button= findViewById(R.id.button)
+        //2. TextView 찾기
+        var tv1 : TextView = findViewById(R.id.textView)
+
+        //3. Button 눌렀을 때를 감지할 Event Listener 달아주기
+        btn1.setOnClickListener {
+            //4. Listener 안에서 TextView의 text 속성 바꾸기
+            //tv1.setText("Good Bye!") --> 메서드 호출 (자바 스타일)
+            tv1.text = "Good Bye!" //Kotlin에서 사용 추천하는 문법!
+            tv1.setTextColor(Color.rgb(50,50,50))
+            //text라는 속성을 변수로 보는 기법
+        }
+
+        var btn2 : Button = findViewById(R.id.button2)
+        btn2.setOnClickListener {
+            tv1.text = "Hello!"
+        }
     }
 }
